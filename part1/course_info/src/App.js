cf9d65c59191d05d206cf15ccb28f87df050efa1
IGimpels <ilia.gimpels@thruinc.com>
2023-01-07 12:15:05 +0400

    ex 1.3

--- part1/course_info/src/App.js
+++ part1/course_info/src/App.js
@@ -7,9 +7,9 @@ const Header = (props) => {
 const Content = (props) => {
   return ( 
     <>
-      <Part title={props.title1} count={props.count1}/>
-      <Part title={props.title2} count={props.count2}/>
-      <Part title={props.title3} count={props.count3}/>    
+      <Part data={props.part1}/>
+      <Part data={props.part2}/>
+      <Part data={props.part3}/>    
     </>
   )
   
@@ -18,7 +18,7 @@ const Content = (props) => {
 const Part = (props) => {
   return (
   <p>
-  {props.title} {props.count}
+  {props.data.name} {props.data.exercises}
   </p>
   )
 }
@@ -31,18 +31,24 @@ const Total = (props) => {
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
 
   return (
     <div>
       <Header name={course}/>
-      <Content title1={part1} count1={exercises1} title2={part2} count2={exercises2} title3={part3} count3={exercises3}/>
-      <Total count={exercises1 + exercises2 + exercises3}/>
+      <Content part1={part1} part2={part2} part3={part3}/>
+      <Total count={part1.exercises + part2.exercises + part3.exercises}/>
     </div>
   )
 }
